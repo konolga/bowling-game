cd94ca21b8223bc7aba1655e847d5e6ae749cb95
Olga <ms.estete@gmail.com>
2021-02-19 20:43:12 +0200

    added API urls

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  userAPI: 'https://localhost:5001/api/Users',
+  scoreAPI: 'https://localhost:5001/api/scores/',
 };
 
 /*
