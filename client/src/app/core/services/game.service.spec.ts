29f036d50b849efa853bb8e0ec9f17761eb3f1b9
Olga <ms.estete@gmail.com>
2021-02-19 20:47:27 +0200

    init game service

--- client/src/app/core/services/game.service.spec.ts
+++ client/src/app/core/services/game.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { DbService } from './db.service';
+import { GameService } from './game.service';
 
-describe('DbService', () => {
-  let service: DbService;
+describe('GameService', () => {
+  let service: GameService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(DbService);
+    service = TestBed.inject(GameService);
   });
 
   it('should be created', () => {
